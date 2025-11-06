baf647aa58deae7df15a53788446c1d632a4262d
Matloob333 <matloobahmad2469@gmail.com>
2025-11-07 00:00:34 +0530

    day2ofjsChe

--- 30 days/day2.js
+++ 30 days/day2.js
@@ -4,6 +4,10 @@ var createCounter = function (n) {
     return n++;
   };
 };
+var counterFrom0 = createCounter(0);
+console.log(counterFrom0()); // 0
+console.log(counterFrom0()); // 1
+console.log(counterFrom0()); // 2
 
 // make a simple expect function for testing
 var expect = function (val) {
