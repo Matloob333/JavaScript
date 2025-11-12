653532ab9041aa635ac8b48a6729c1a75e1619b1
Matloob333 <matloobahmad2469@gmail.com>
2025-11-13 02:06:01 +0530

    minor change

--- functions/script.js
+++ functions/script.js
@@ -60,18 +60,56 @@ const obj1 = {
 console.log(obj1.greet(obj.name));
 
 // async function
-async function fetchData() {
-  let response = await fetch("https://github.com/Matloob333");
-  let data = await response.text(); // or use response.json() if API returns JSON
-  console.log(data);
-  return data;
-}
-fetchData();
+// async function fetchData() {
+//   let response = await fetch("https://github.com/Matloob333");
+//   let data = await response.text(); // or use response.json() if API returns JSON
+//   console.log(data);
+//   return data;
+// }
+// fetchData();
 
-async function fetchRepos() {
-  let response = await fetch("https://api.github.com/users/Matloob333/repos");
-  let data = await response.json();
-  console.log(data);
-  return data;
+// async function fetchRepos() {
+//   let response = await fetch("https://api.github.com/users/Matloob333/repos");
+//   let data = await response.json();
+//   console.log(data);
+//   return data;
+// }
+// fetchRepos();
+
+//print print 1 to 5 every 1 second interval
+function count() {
+  for (let i = 1; i <= 5; i++) {
+    setTimeout(() => {
+      console.log("print 1 to 5 every 1 second interval:", i);
+    }, i * 1000);
+  }
 }
-fetchRepos();
+count();
+
+// map in functions
+
+const radius = [3, 1, 2, 4];
+
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+  return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius) {
+  return 2 * radius;
+};
+
+const calculate = function (radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+};
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
