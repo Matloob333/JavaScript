7615247c149df8330feec894e825c7574fff7072
Matloob333 <matloobahmad2469@gmail.com>
2025-11-24 23:30:38 +0530

    day16OfJsChallenge

--- 30 days/day2.js
+++ 30 days/day2.js
@@ -305,3 +305,34 @@ const cancelLogInterval = cancellable(
 setTimeout(() => {
   cancelLogInterval();
 }, 10000);
+
+function timeLimit(fn, t) {
+  return async function (...args) {
+    return Promise.race([
+      fn(...args),
+      new Promise((_, reject) =>
+        setTimeout(() => reject("Time Limit Exceeded"), t)
+      ),
+    ]);
+  };
+}
+// Core snippet:
+var TimeLimitedCache = function () {
+  this.cache = new Map();
+};
+
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  const exists = this.cache.has(key);
+  if (exists) clearTimeout(this.cache.get(key).timeout);
+  const timeout = setTimeout(() => this.cache.delete(key), duration);
+  this.cache.set(key, { value, timeout });
+  return exists;
+};
+
+TimeLimitedCache.prototype.get = function (key) {
+  return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
